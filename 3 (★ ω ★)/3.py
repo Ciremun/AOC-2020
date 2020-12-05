fe11fbf7aaa8cdc3bd14aaca285d2fd69a793d1c
Ciremun <Ciremun@yandex.ru>
2020-12-05 22:56:36 +0300

    refactor

--- 3 (★ ω ★)/3.py
+++ 3 (★ ω ★)/3.py
@@ -2,22 +2,22 @@ from typing import List, Iterable
 
 # how many trees would you encounter?
 
-def get_trees(patterns: List[str], *, right: int, down=1) -> int:
-    down -= 1
+def get_trees(patterns: List[str], *, right: int, down: int) -> int:
+    skip = 0
     cursor_pos = 0
     trees_count = 0
-    skip = 0
-    for pattern in patterns[down+1:]:
+    do_skip = down - 1
+    for pattern in patterns[down:]:
         if skip:
             skip -= 1
             continue
         cursor_pos += right
-        while cursor_pos > len(pattern) - 1:
+        while cursor_pos >= len(pattern):
             pattern += pattern
         if pattern[cursor_pos] == '#':
             trees_count += 1
-        if down:
-            skip = down
+        if do_skip:
+            skip = do_skip
     return trees_count
 
 def multiply(iterable: Iterable):
@@ -27,14 +27,13 @@ def multiply(iterable: Iterable):
     return prod
 
 def main():
-    inp = open('input.txt').read()
-    lines = inp.split('\n')
-    trees = get_trees(lines, right=3)
-    print(f'pt.1: {trees} trees encountered')
+    lines = open('input.txt').read().split('\n')
+    result_p1 = get_trees(lines, right=3, down=1)
     pt2_args = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-    trees = [get_trees(lines, right=args[0], down=args[1])
-             for args in pt2_args]
-    print(f'pt.2: {multiply(trees)} trees multiplied together, {", ".join(str(x) for x in trees)} trees encountered')
+    result_p2 = [get_trees(lines, right=args[0], down=args[1]) for args in pt2_args]
+    print(f'trees:\npt.1: {result_p1} encountered\n'
+          f'pt.2: {", ".join(str(x) for x in result_p2)} encountered, '
+          f'{multiply(result_p2)} multiplied together')
 
 if __name__ == '__main__':
     main()
