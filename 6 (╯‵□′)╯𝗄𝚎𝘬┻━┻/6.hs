e9a714ee40648975bbdf6087b17d1b04365143b4
Ciremun <Ciremun@yandex.ru>
2020-12-07 17:30:13 +0300

    6: pt.2

--- 6 (╯‵□′)╯𝗄𝚎𝘬┻━┻/6.hs
+++ 6 (╯‵□′)╯𝗄𝚎𝘬┻━┻/6.hs
@@ -1,11 +1,24 @@
 import Data.List
 import Text.Printf
 
--- Thank you, Funkschy!
+-- Thank you, rexim!
+tail' :: [a] -> [a]
+tail' (_:xs) = xs
+tail' [] = []
 
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn d s = x:splitOn d (tail' s')
+  where (x, s') = span (/= d) s
+--
+
+-- Thank you, Funkschy!
 main :: IO ()
 main = do
   filecontent <- readFile "input.txt"
   let filelines = lines filecontent
   let pt1 = sum $ map (length . nub . sort) $ words $ concatMap (\c -> if c == "" then " " else c) filelines
-  printf "%s %d" "pt.1:" pt1
+  let pt2 = sum $ map (length . foldl1 intersect) $ splitOn "" filelines
+  printf "pt.1: %d\n" pt1
+  printf "pt.2: %d" pt2
+--
